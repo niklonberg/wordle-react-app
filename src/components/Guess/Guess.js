21750ff06e981c48578dd2b8bf1e08c8918a482e
Nikolai Lønberg <niklonberg@gmail.com>
2024-03-22 16:59:12 +0100

    add onSubmit & onChange events, attributes to input. guess is always converted to upper case for easier comparison, constrained input to max 5 letters

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -3,14 +3,28 @@ import styles from "./Guess.module.scss";
 
 function Guess() {
   const [guess, setGuess] = React.useState("");
+
+  function handleSubmitGuess(event) {
+    event.preventDefault();
+    console.log(guess);
+    setGuess("");
+  }
+
   return (
-    <form className={styles["guess-input-wrapper"]}>
+    <form
+      className={styles["guess-input-wrapper"]}
+      onSubmit={(event) => handleSubmitGuess(event)}
+    >
       <label htmlFor="guess-input">Enter guess:</label>
       <input
         id="guess-input"
-        value={guess}
         type="text"
-        onChange={(event) => setGuess(event.target.value)}
+        minLength={5}
+        maxLength={5}
+        pattern="[a-zA-Z]{5}"
+        title="Please enter exactly 5 letters."
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
       ></input>
       <button>Submit</button>
     </form>
