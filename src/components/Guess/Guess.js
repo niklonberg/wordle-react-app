5384f27be9c5684056a2c50ddb3b101edcbebeaf
Nikolai Lønberg <niklonberg@gmail.com>
2024-03-22 16:41:34 +0100

    add .guess-input-wrapper, add button to form

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,15 +1,18 @@
 import React from "react";
+import styles from "./Guess.module.scss";
 
 function Guess() {
   const [guess, setGuess] = React.useState("");
   return (
-    <form className>
-      <label htmlFor="guess">Enter guess:</label>
+    <form className={styles["guess-input-wrapper"]}>
+      <label htmlFor="guess-input">Enter guess:</label>
       <input
-        id="guess"
+        id="guess-input"
         value={guess}
+        type="text"
         onChange={(event) => setGuess(event.target.value)}
       ></input>
+      <button>Submit</button>
     </form>
   );
 }
