091bf6b38658a9eae269119b8cd70da7c8e28426
Nikolai Lønberg <niklonberg@gmail.com>
2024-03-23 11:02:18 +0100

    add Game component, use in App, remove Guess component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,12 @@
 import styles from "./App.module.scss";
 import Header from "../Header/Header";
-import Guess from "../Guess/Guess";
+import Game from "../Game/Game";
 
 export function App() {
   return (
     <div className={styles.wrapper}>
       <Header />
-      <div className={styles.gameWrapper}>{/* <Game /> */}</div>
-      <Guess />
+      <div className={styles.gameWrapper}>{<Game />}</div>
     </div>
   );
 }
---
+++ src/components/Game/Game.js
@@ -0,0 +1,5 @@
+function Game() {
+  return <p>hello</p>;
+}
+
+export default Game;
