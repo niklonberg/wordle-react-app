a9ea283ffd091b530fca3f25cf94d9abd00e737e
Nikolai Lønberg <niklonberg@gmail.com>
2024-03-23 11:10:04 +0100

    add getting wordToGuess on every page load

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,3 +1,13 @@
+import React from "react";
+import Guess from "../Guess/Guess";
+
+import { randomSelector } from "../../utilities";
+import { WORDS } from "../../guesswords";
+
+const wordToGuess = randomSelector(WORDS);
+
+console.log({ wordToGuess });
+
 function Game() {
   return <p>hello</p>;
 }
